52f778a23122bb94295f9ca81f5b485b3c8277c6
qs_theme <qstxdy@gmail.com>
2024-10-11 10:40:28 +0800

    none

--- .vitepress/config.mjs
+++ .vitepress/config.mjs
@@ -35,10 +35,18 @@ export default defineConfig({
 					short_name: 'blog',
 					description: '日志',
 					theme_color: '#ffffff',
+					background_color: '#ffffff',
+					display: 'standalone',
+					start_url: '/',
 					icons: [
 						{
 							src: 'icon-192x192.png',
 							sizes: '192x192',
+							type: 'image/png',
+						},
+						{
+							src: 'icon-512x512.png',
+							sizes: '512x512',
 							type: 'image/png',
 						},
 					],
